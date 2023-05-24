2d0bb02bd87a9690f572a1ef7619a6635f166c0b
joao teodoro <joaotdn@gmail.com>
2023-05-24 18:32:43 -0300

    feat: configuring header

--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,39 @@
-<header class="ps-header container-fluid px-0">
-    
+<header class="ps-header container-fluid px-0 py-2 bg-dark border-bottom border-danger border-2">
+    <div class="container ps-top-bar">
+        <div class="row d-flex justify-content-between">
+            <div class="col text-white-50">
+                <span class="temp-max"></span>
+                <span class="temp-min d-inline-block ps-1"></span>
+                <span class="current-city d-inline-block ps-2"></span>
+                <span class="current-date text-lowercase"></span>
+            </div>
+
+            <div class="col ps-top-tags">
+                <ul class="nav justify-content-center py-0">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0">Cajazeiras</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0">Eleições</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0">Cultura</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0">Exclusivo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0">Apelo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0">Neligencia</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col ps-top-social">
+                
+            </div>
+        </div>
+    </div>
 </header>
