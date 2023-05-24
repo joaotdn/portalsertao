b38e7b5b4ec0924bfa87110f408844b2fb25f9b0
joao teodoro <joaotdn@gmail.com>
2023-05-24 18:55:42 -0300

    add social bar on header

--- header.php
+++ header.php
@@ -12,4 +12,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-    
+    <?php
+    get_template_part('parts/header');
+    ?>
--- parts/header.php
+++ parts/header.php
@@ -1,14 +1,14 @@
 <header class="ps-header container-fluid px-0 py-2 bg-dark border-bottom border-danger border-2">
     <div class="container ps-top-bar">
         <div class="row d-flex justify-content-between">
-            <div class="col text-white-50">
+            <div class="col-auto text-white-50">
                 <span class="temp-max"></span>
                 <span class="temp-min d-inline-block ps-1"></span>
                 <span class="current-city d-inline-block ps-2"></span>
                 <span class="current-date text-lowercase"></span>
             </div>
 
-            <div class="col ps-top-tags">
+            <div class="col-auto ps-top-tags">
                 <ul class="nav justify-content-center py-0">
                     <li class="nav-item">
                         <a href="#" class="nav-link text-white px-2 py-0">Cajazeiras</a>
@@ -31,8 +31,34 @@
                 </ul>
             </div>
 
-            <div class="col ps-top-social">
-                
+            <div class="col-auto ps-top-social">
+                <ul class="nav justify-content-end py-0">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            </i><i class="fa-brands fa-youtube"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
