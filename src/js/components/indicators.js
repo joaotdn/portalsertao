7793e30f7fa498cc750fbde7b45216abbcded147
joao teodoro <joaotdn@gmail.com>
2023-06-07 03:27:26 -0300

    feat: add economic indicators

--- src/js/components/indicators.js
+++ src/js/components/indicators.js
@@ -1,20 +1,16 @@
 (function ($) {
-    if ($('.economic-indicators').length) {
+    if ($('.ps-coins-indicators').length) {
         $.ajax({
             url: 'http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL',
             success: function (data) {
-                $('.cur-usd > .cur-min').html('$ ' + parseFloat(data.USDBRL.low).toFixed(2));
-                $('.cur-usd > .cur-max').html('$ ' + parseFloat(data.USDBRL.high).toFixed(2));
+                $('.cur-usd > .cur-min').html(parseFloat(data.USDBRL.low).toFixed(2));
+                $('.cur-usd > .cur-max').html(parseFloat(data.USDBRL.high).toFixed(2));
 
-                $('.cur-euro > .cur-min').html('€ ' + parseFloat(data.EURBRL.low).toFixed(2));
-                $('.cur-euro > .cur-max').html('€ ' + parseFloat(data.EURBRL.high).toFixed(2));
+                $('.cur-euro > .cur-min').html(parseFloat(data.EURBRL.low).toFixed(2));
+                $('.cur-euro > .cur-max').html(parseFloat(data.EURBRL.high).toFixed(2));
 
-                $('.cur-btc > .cur-min').html('₿ ' + parseFloat(data.BTCBRL.low).toFixed(3));
-                $('.cur-btc > .cur-max').html('₿ ' + parseFloat(data.BTCBRL.high).toFixed(3));
-            },
-            complete: function () {
-                $('.economic-indicators').find('.list-group').removeClass('invisible');
-                $('.indicators-loader').addClass('d-none');
+                $('.cur-btc > .cur-min').html(parseFloat(data.BTCBRL.low).toFixed(3));
+                $('.cur-btc > .cur-max').html(parseFloat(data.BTCBRL.high).toFixed(3));
             }
         });
     }
