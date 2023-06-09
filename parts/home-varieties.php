7a9bea49e6ba93a9568e142cbf5b2e61df238525
joao teodoro <joaotdn@gmail.com>
2023-06-09 12:46:15 -0300

    feat: add more read articles

--- parts/home-varieties.php
+++ parts/home-varieties.php
@@ -6,7 +6,7 @@
                     <div class="col-12">
                         <div class="w-100 ps-home-news--header d-flex justify-content-between mb-4">
                             <a href="#" title="Ver todas as notícias em Municípios" class="text-danger">
-                                <h5>Municípios</h5>
+                                <h5 class="font-title">Municípios</h5>
                             </a>
                             <nav class="ps-home-news--tags nav d-none d-lg-flex">
                                 <a href="#" title="" class="nav-link">Cajazeiras</a>
@@ -17,7 +17,7 @@
                             </nav>
                         </div>
                     </div>
-                    
+
                     <div class="col-12 col-md-6">
                         <a href="#" class="d-inline-block border-bottom border-2 border-danger mb-2" title="">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/v1.jpg" alt="">
@@ -25,7 +25,7 @@
                         <p class="font-tag"><i class="fa-solid fa-location-dot"></i> Sousa</p>
                         <a href="#" title="">
                             <h5 class="font-title">
-                            Definida programação de Corpus Christi que será celebrada pela Igreja Católica em Cajazeiras
+                                Definida programação de Corpus Christi que será celebrada pela Igreja Católica em Cajazeiras
                             </h5>
                         </a>
                     </div>
@@ -48,6 +48,76 @@
                             </a>
                         </nav>
                     </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="row">
+                    <div class="col-12 ps-home-news--authors">
+                        <div class="w-100 border-bottom border-2 mb-4">
+                            <h5 class="font-title">Colunas</h5>
+                        </div>
+
+                        <nav class="nav flex-column">
+                            <div class="row mb-3">
+                                <div class="col-auto">
+                                    <div class="pe-3 author-th bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/c1.jpg"></div>
+                                </div>
+                                <div class="col">
+                                    <a href="#" title="" class="font-tag">Margarida Araújo</a>
+                                    <a href="#" title="">
+                                        <h6 class="font-title">Os lobos de cada um</h6>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-auto">
+                                    <div class="pe-3 author-th bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/c2.jpg"></div>
+                                </div>
+                                <div class="col">
+                                    <a href="#" title="" class="font-tag">Wgleyson de Souza</a>
+                                    <a href="#" title="">
+                                        <h6 class="font-title">Susto</h6>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-auto">
+                                    <div class="pe-3 author-th bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/c3.jpg"></div>
+                                </div>
+                                <div class="col">
+                                    <a href="#" title="" class="font-tag">José Antonio de Albuquerque</a>
+                                    <a href="#" title="">
+                                        <h6 class="font-title">Resultado preliminar do concurso da Câmara de Cajazeiras é divulgado; confira!</h6>
+                                    </a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="w-100 bg-light p-3">
+                    <div class="w-100 border-bottom border-2 my-3 rounded-3">
+                        <h5 class="font-title text-danger"><i class="fa-solid fa-ranking-star"></i> Mais lidas</h5>
+                    </div>
+                    <nav class="nav ps-home-news--list">
+                        <a href="#" class="nav-link px-0 d-block" title="">
+                            <p class="font-tag">Uirauna</p>
+                            <h6 class="font-title">Todo mundo diz que UTI Aérea é coisa de rico. Pois na PB é diferente', avalia Nonato Bandeira sobre mais uma UTI e 40 ambulâncias</h6>
+                        </a>
+
+                        <a href="#" class="nav-link px-0 d-block" title="">
+                            <p class="font-tag">São João</p>
+                            <h6 class="font-title">Em Brasília: prefeito Zé Aldemir tenta liberar recursos de obras conveniadas com o governo federal</h6>
+                        </a>
+
+                        <a href="#" class="nav-link px-0 d-block" title="">
+                            <p class="font-tag">São José de Piranhas</p>
+                            <h6 class="font-title">Assembleia aprova pedido de Doutora Paula ao TJPB para elevação do Município de Cajazeiras a terceira entrância</h6>
+                        </a>
+                    </nav>
                 </div>
             </div>
         </div>
