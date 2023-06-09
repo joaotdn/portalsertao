6439d45471645440f3ba93692ef1021b35746507
joao teodoro <joaotdn@gmail.com>
2023-06-09 11:17:13 -0300

    add cities section

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 get_header();
+
 get_template_part('parts/home-features');
 get_template_part('parts/home-videos');
+get_template_part('parts/home-varieties');
+
 get_footer()
 ?>
--- parts/home-varieties.php
+++ parts/home-varieties.php
@@ -1,110 +1,52 @@
-<?php
-$category_id = get_cat_ID('Variedades');
-$posts = get_posts(array(
-    'posts_per_page' => 4,
-    'cat' => $category_id,
-    'meta_query'     => array(
-        'relation'        => 'AND',
-        array(
-            'key'        => 'epb_destaques',
-            'value'      => 'destaque1',
-            'compare'    => '!='
-        ),
-        array(
-            'key'        => 'epb_destaques',
-            'value'      => 'destaque2',
-            'compare'    => '!='
-        ),
-        array(
-            'key'        => 'epb_destaques',
-            'value'      => 'destaque3',
-            'compare'    => '!='
-        )
-    )
-));
-$tags = get_category_tags(array('categories' => $category_id));
-?>
-<section class="epb-varieties epb-section container">
-    <header class="row epb-section__header mb-4">
-        <div class="col-12 d-flex justify-content-between border-bottom ">
-            <h1 class="epb-section__header--title font-title fs-4 text-info fw-semibold m-0">
-                <a href="<?php echo get_category_link($category_id); ?>" title="Ver mais notícias em Variedades" class="text-info">Variedades</a>
-            </h1>
-            <ul class="nav epb-section__header--tags m-0 ps-4 d-none d-md-flex">
-                <?php foreach ($tags as $tag) : ?>
-                    <li class="ps-1 nav-item">
-                        <a href="<?php echo $tag->tag_link; ?>" title="Ir para <?php echo $tag->tag_name; ?>" class="badge text-bg-info fw-light"><?php echo $tag->tag_name; ?></a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-            <a href="<?php echo get_category_link($category_id); ?>" title="Leia mais notícias em Variedades" class="ms-auto text-info">
-                <i class="fa-solid fa-list"></i>
-            </a>
-        </div>
-    </header>
-
-    <div class="row">
-        <div class="col-12 col-md-8">
-            <div class="row">
-                <div class="col-12 col-md-6 mb-3">
-                    <a href="<?php echo get_the_permalink($posts[0]->ID); ?>" title="<?php echo $posts[0]->post_title; ?>">
-                        <img src="<?php echo get_the_post_thumbnail_url($posts[0]->ID, 'ebp-thumb-horizontally'); ?>" alt="<?php echo $posts[0]->post_title; ?>">
-                    </a>
-                    <p class="mb-0 mt-3 font-tag">
-                        <?php echo wp_get_post_tags($posts[0]->ID)[0]->name; ?>
-                    </p>
-                    <h1 class="font-title fs-6">
-                        <a href="<?php echo get_the_permalink($posts[0]->ID); ?>" title="<?php echo $posts[0]->post_title; ?>"><?php echo $posts[0]->post_title; ?></a>
-                    </h1>
-                </div>
-
-                <div class="col-12 col-md-6 mb-3">
-                    <a href="<?php echo get_the_permalink($posts[1]->ID); ?>" title="<?php echo $posts[1]->post_title; ?>">
-                        <img src="<?php echo get_the_post_thumbnail_url($posts[1]->ID, 'ebp-thumb-horizontally'); ?>" alt="<?php echo $posts[1]->post_title; ?>">
-                    </a>
-                    <p class="mb-0 mt-3 font-tag">
-                        <?php echo wp_get_post_tags($posts[1]->ID)[0]->name; ?>
-                    </p>
-                    <h1 class="font-title fs-6">
-                        <a href="<?php echo get_the_permalink($posts[1]->ID); ?>" title="<?php echo $posts[1]->post_title; ?>"><?php echo $posts[1]->post_title; ?></a>
-                    </h1>
-                </div>
-
-                <div class="col-12 col-md-6 mb-3">
-                    <p class="related-link d-flex align-items-stretchs fs-6 fs-lg-4">
-                        <span class="related-link__icon d-inline-block">
-                            <i class="fa-solid fa-arrow-right-to-bracket text-primary"></i>
-                        </span>
-                        <a href="<?php echo get_the_permalink($posts[2]->ID); ?>" title="<?php echo $posts[2]->post_title; ?>" class="d-inline-block ps-2">
-                            <small>
-                                <?php echo $posts[2]->post_title; ?>
-                            </small>
+<section class="ps-home-news container-fluid my-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="w-100 ps-home-news--header d-flex justify-content-between mb-4">
+                            <a href="#" title="Ver todas as notícias em Municípios" class="text-danger">
+                                <h5>Municípios</h5>
+                            </a>
+                            <nav class="ps-home-news--tags nav d-none d-lg-flex">
+                                <a href="#" title="" class="nav-link">Cajazeiras</a>
+                                <a href="#" title="" class="nav-link">Sousa</a>
+                                <a href="#" title="" class="nav-link">Campina Grande</a>
+                                <a href="#" title="" class="nav-link">Pombal</a>
+                                <a href="#" title="" class="nav-link">Cachoeira dos Índios</a>
+                            </nav>
+                        </div>
+                    </div>
+                    
+                    <div class="col-12 col-md-6">
+                        <a href="#" class="d-inline-block border-bottom border-2 border-danger mb-2" title="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/v1.jpg" alt="">
                         </a>
-                    </p>
-                </div>
-
-                <div class="col-12 col-md-6 mb-3">
-                    <p class="related-link d-flex align-items-stretchs fs-6 fs-lg-4">
-                        <span class="related-link__icon d-inline-block">
-                            <i class="fa-solid fa-arrow-right-to-bracket text-primary"></i>
-                        </span>
-                        <a href="<?php echo get_the_permalink($posts[3]->ID); ?>" title="<?php echo $posts[3]->post_title; ?>" class="d-inline-block ps-2">
-                            <small>
-                                <?php echo $posts[3]->post_title; ?>
-                            </small>
+                        <p class="font-tag"><i class="fa-solid fa-location-dot"></i> Sousa</p>
+                        <a href="#" title="">
+                            <h5 class="font-title">
+                            Definida programação de Corpus Christi que será celebrada pela Igreja Católica em Cajazeiras
+                            </h5>
                         </a>
-                    </p>
-                </div>
-            </div>
-        </div>
+                    </div>
 
-        <div class="col-12 col-md-4">
-            <div class="row">
-                <div class="col-12">
-                    <div class="features-sidebar-ads text-center">
-                        <figure class="ads--300 border bg-light d-inline-block">
-                            <span class="text-"><small>Publicidade</small></span>
-                        </figure>
+                    <div class="col-12 col-md-6">
+                        <nav class="nav ps-home-news--list">
+                            <a href="#" class="nav-link px-0 d-block" title="">
+                                <p class="font-tag"><i class="fa-solid fa-location-dot"></i> Uirauna</p>
+                                <h6 class="font-title">Todo mundo diz que UTI Aérea é coisa de rico. Pois na PB é diferente', avalia Nonato Bandeira sobre mais uma UTI e 40 ambulâncias</h6>
+                            </a>
+
+                            <a href="#" class="nav-link px-0 d-block" title="">
+                                <p class="font-tag"><i class="fa-solid fa-location-dot"></i> São João</p>
+                                <h6 class="font-title">Em Brasília: prefeito Zé Aldemir tenta liberar recursos de obras conveniadas com o governo federal</h6>
+                            </a>
+
+                            <a href="#" class="nav-link px-0 d-block" title="">
+                                <p class="font-tag"><i class="fa-solid fa-location-dot"></i> São José de Piranhas</p>
+                                <h6 class="font-title">Assembleia aprova pedido de Doutora Paula ao TJPB para elevação do Município de Cajazeiras a terceira entrância</h6>
+                            </a>
+                        </nav>
                     </div>
                 </div>
             </div>
