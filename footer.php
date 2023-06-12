fdaf0691a92f0093a55d3025de7856c03698e952
joao teodoro <joaotdn@gmail.com>
2023-06-12 04:18:09 -0300

    feat: add footer

--- footer.php
+++ footer.php
@@ -1,8 +1,99 @@
-<footer id="ps-footer" class="container-fluid bg-dark py-4 mt-4">
+<footer class="ps-footer container-fluid bg-dark py-4 mt-4 border-top border-4 border-black">
     <div class="container">
-        <div class="row">
-            <div class="col"></div>
+        <div class="row d-flex">
+            <div class="col-12 col-md-4">
+                <a href="#" title="Voltar pra página principal">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ps-logo-small.png" alt="Marca do Portal Sertão">
+                </a>
+                <ul class="nav justify-content-start py-0 my-3">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            </i><i class="fa-brands fa-youtube"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white px-2 py-0 text-white-50">
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-8">
+                <div class="row">
+                    <div class="col-12 col-md-4 mb-3 mb-md-0">
+                        <div class="w-100 d-flex justify-content-md-end">
+                            <nav class="nav flex-column ps-footer--nav">
+                                <h6 class="text-white font-title"><i class="fa-solid fa-align-left"></i> Editoriais</h6>
+                                <a class="nav-link" href="#">Entretenimento</a>
+                                <a class="nav-link" href="#">Política</a>
+                                <a class="nav-link" href="#">Paraíba</a>
+                                <a class="nav-link" href="#">Manchetes</a>
+                                <a class="nav-link" href="#">Polícia</a>
+                                <a class="nav-link" href="#">Economia</a>
+                                <a class="nav-link" href="#">Esportes</a>
+                                <a class="nav-link" href="#">Saúde</a>
+                                <a class="nav-link" href="#">Tecnologia</a>
+                                <a class="nav-link" href="#">Educação</a>
+                                <a class="nav-link" href="#">Brasil</a>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-4 mb-3 mb-md-0">
+                        <div class="w-100 d-flex justify-content-md-end">
+                            <nav class="nav flex-column ps-footer--nav">
+                                <h6 class="text-white font-title"><i class="fa-solid fa-location-arrow"></i> Municípios</h6>
+                                <a class="nav-link" href="#">Água Branca - PB</a>
+                                <a class="nav-link" href="#">Aguiar - PB</a>
+                                <a class="nav-link" href="#">Alagoa Grande - PB</a>
+                                <a class="nav-link" href="#">Alagoa Nova - PB</a>
+                                <a class="nav-link" href="#">Alagoinha - PB</a>
+                                <a class="nav-link" href="#">Alcantil - PB</a>
+                                <a class="nav-link" href="#">Algodão de Jandaíra - PB</a>
+                                <a class="nav-link" href="#">Alhandra - PB</a>
+                                <a class="nav-link" href="#">Amparo - PB</a>
+                                <a class="nav-link" href="#">Cajazeiras - PB</a>
+                                <a class="nav-link" href="#"><strong>ver todas</strong></a>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-4 mb-3 mb-md-0">
+                        <div class="w-100 d-flex justify-content-md-end">
+                            <nav class="nav flex-column ps-footer--nav">
+                                <h6 class="text-white font-title"><i class="fa-solid fa-quote-left"></i> Colunas</h6>
+                                <a class="nav-link" href="#">Alexandre Costa</a>
+                                <a class="nav-link" href="#">CALDEIRÃO POLÍTICO</a>
+                                <a class="nav-link" href="#">Jornal Gazeta do Alto Piranhas</a>
+                                <a class="nav-link" href="#">José Antônio de Albuquerque</a>
+                                <a class="nav-link" href="#">Margarida Araújo</a>
+                                <a class="nav-link" href="#">Rui Leitão</a>
+                                <a class="nav-link" href="#">WGLEYSON DE SOUZA</a>
+                                <a class="nav-link" href="#"><strong>ver todas</strong></a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="ps-footer--credits w-100 p-4 text-center mt-4 pb-0">
+        <p class="m-0">&copy; 2023 - <a href="#" title="" class="text-white">Portal Sertão</a>. Todos os direitos reservados</p>
     </div>
 </footer>
 <?php
--- parts/home-varieties.php
+++ parts/home-varieties.php
@@ -126,7 +126,7 @@
                     </div>
                     
                     <div class="col-12 col-md-4">
-                        <div class="w-100 bg-danger px-3 pt-2 border-top border-4 border-dark mb-3">
+                        <div class="w-100 bg-danger px-3 pt-3 border-top border-4 border-dark mb-3">
                             <div class="w-100 my-2 mb-3">
                                 <h5 class="font-title text-dark"><i class="fa-solid fa-camera"></i> Galerias</h5>
                             </div>
@@ -136,6 +136,7 @@
                                         <div class="pe-3 thumb-gallery bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/g1.jpg"></div>
                                     </div>
                                     <div class="col">
+                                        <p class="m-0"><small>Cajazeiras</small></p>
                                         <a href="#" title="" class="text-white">
                                             <h6 class="font-title">Assembleia aprova pedido de Doutora Paula ao TJPB para elevação do Município de Cajazeiras a terceira entrância</h6>
                                         </a>
@@ -147,6 +148,7 @@
                                         <div class="pe-3 thumb-gallery bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/g2.jpg"></div>
                                     </div>
                                     <div class="col">
+                                        <p class="m-0"><small>Brasília</small></p>
                                         <a href="#" title="" class="text-white">
                                             <h6 class="font-title">Em Brasília: prefeito Zé Aldemir tenta liberar recursos de obras conveniadas com o governo federal</h6>
                                         </a>
@@ -158,6 +160,7 @@
                                         <div class="pe-3 thumb-gallery bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/g3.jpg"></div>
                                     </div>
                                     <div class="col">
+                                        <p class="m-0"><small>UTI Aérea</small></p>
                                         <a href="#" title="" class="text-white">
                                             <h6 class="font-title">Todo mundo diz que UTI Aérea é coisa de rico. Pois na PB é diferente', avalia Nonato Bandeira sobre mais uma UTI e 40 ambulâncias</h6>
                                         </a>
@@ -183,10 +186,9 @@
                 </div>
             </div>
 
-
             <div class="col-12 col-md-4">
                 <a href="#" title="" class="w-100 ps-media-news my-2 bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/g1.jpg">
-                    <span class="ps-media-news--tag d-inline-block p-2 bg-danger text-light text-uppercase">Economia</span>
+                    <span class="ps-media-news--tag d-inline-block p-2 bg-dark text-light text-uppercase"><i class="fa-regular fa-folder"></i> Política</span>
                     <h5 class="font-title ps-media-news--title d-inline-block text-light p-2 w-100">Assembleia aprova pedido de Doutora Paula ao TJPB para elevação do Município de Cajazeiras a terceira entrância</h5>
                     <span class="ps-media-news--mask d-block"></span>
                 </a>
@@ -194,7 +196,7 @@
 
             <div class="col-12 col-md-4">
                 <a href="#" title="" class="w-100 ps-media-news my-2 bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/g2.jpg">
-                    <span class="ps-media-news--tag d-inline-block p-2 bg-danger text-light text-uppercase">Entretenimento</span>
+                    <span class="ps-media-news--tag d-inline-block p-2 bg-dark text-light text-uppercase"><i class="fa-regular fa-folder"></i> Esportes</span>
                     <h5 class="font-title ps-media-news--title d-inline-block text-light p-2 w-100">
                         Em Brasília: prefeito Zé Aldemir tenta liberar recursos de obras conveniadas com o governo federal</h5>
                     <span class="ps-media-news--mask d-block"></span>
@@ -203,10 +205,66 @@
 
             <div class="col-12 col-md-4">
                 <a href="#" title="" class="w-100 ps-media-news my-2 bg-cover d-inline-block" data-thumb-post="<?php echo get_template_directory_uri(); ?>/assets/img/g3.jpg">
-                    <span class="ps-media-news--tag d-inline-block p-2 bg-danger text-light text-uppercase">Policial</span>
+                    <span class="ps-media-news--tag d-inline-block p-2 bg-dark text-light text-uppercase"><i class="fa-regular fa-folder"></i> Policial</span>
                     <h5 class="font-title ps-media-news--title d-inline-block text-light p-2 w-100">Todo mundo diz que UTI Aérea é coisa de rico. Pois na PB é diferente', avalia Nonato Bandeira sobre mais uma UTI e 40 ambulâncias</h5>
                     <span class="ps-media-news--mask d-block"></span>
                 </a>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="row ps-home-news--footer my-3">
+                    <div class="col-auto pe-0">
+                        <i class="fa-regular fa-folder"></i>
+                    </div>
+                    <div class="col">
+                        <a href="#" title="" class="font-tag">Entretenimento</a>
+                        <a href="#" title="">
+                            <h6>Governo autoriza projeto da ponte sobre o Rio Piranhas; Prefeito Bal Lins e Chico Mendes agradecem gesto de atenção de João Azevêdo</h6>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="row ps-home-news--footer my-3">
+                    <div class="col-auto pe-0">
+                        <i class="fa-regular fa-folder"></i>
+                    </div>
+                    <div class="col">
+                        <a href="#" title="" class="font-tag">Paraíba</a>
+                        <a href="#" title="">
+                            <h6>Prefeito Zé Aldemir entrega mais duas escolas reformadas e ampliadas em Cajazeiras</h6>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="row ps-home-news--footer my-3">
+                    <div class="col-auto pe-0">
+                        <i class="fa-regular fa-folder"></i>
+                    </div>
+                    <div class="col">
+                        <a href="#" title="" class="font-tag">Saúde</a>
+                        <a href="#" title="">
+                            <h6>Em Cajazeiras, será feriado de Corpus Christi quinta (08) e sexta (09) terá ponto facultativo nas repartições municipais</h6>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="row ps-home-news--footer my-3">
+                    <div class="col-auto pe-0">
+                        <i class="fa-regular fa-folder"></i>
+                    </div>
+                    <div class="col">
+                        <a href="#" title="" class="font-tag">Brasil</a>
+                        <a href="#" title="">
+                            <h6>Em Brasília: prefeito Zé Aldemir tenta liberar recursos de obras conveniadas com o governo federal</h6>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
