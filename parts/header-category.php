960d18f1a2f65214696c6824b1b7a14d89346a05
joao teodoro <joaotdn@gmail.com>
2023-07-15 14:28:37 -0300

    add category page

---
+++ parts/header-category.php
@@ -0,0 +1,61 @@
+<?php
+$obj = get_queried_object();
+?>
+<div class="ps-top-bar container-fluid px-0 py-2 bg-black border-bottom border-dark border-2 d-none d-lg-block">
+    <div class="container">
+        <?php get_template_part('parts/top-bar'); ?>
+    </div>
+</div>
+
+<header class="container-fluid ps-header bg-black border-bottom border-danger border-2">
+    <div class="container">
+        <div class="row ps-header-content py-3">
+            <div class="col-12 d-flex align-items-center">
+                <div>
+                    <a href="#" class="ps-toggle-offcanvas ps-toggle-menu text-light me-3 is-single">
+                        <i class="fa-solid fa-bars"></i>
+                    </a>
+                </div>
+                <div>
+                    <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ps-logo.png" alt="Marca do Portal Sertão" class="ps-header-logo" width="140">
+                    </a>
+                </div>
+
+                <div class="text-center flex-grow-1">
+                    <h2 class="font-title m-0 text-white"><i class="fa-regular fa-folder"></i> <?php echo $obj->name; ?></h2>
+                </div>
+
+                <div>
+                    <div class="w-100">
+                        <div class="flex-shrink-1 text-white position-relative ps-toggle-search is-single">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <form action="<?php echo home_url('/'); ?>" class="ps-form-search-mo w-100 p-2 mt-3" role="search" method="get">
+                    <div class="input-group input-group-sm">
+                        <input type="search" class="form-control" placeholder="Buscar" name="s">
+                        <button class="btn btn-outline-light" type="submit">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container-fluid bg-dark ps-search-content">
+    <div class="container">
+        <div class="row">
+            <div class="col py-3">
+                <form action="<?php echo home_url('/'); ?>" role="search" method="get" class="ps-form-search w-100">
+                    <input type="search" class="w-100 form-control form-control-sm" name="s">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ sidebar-category.php
@@ -0,0 +1,48 @@
+<?php
+$obj = get_queried_object();
+
+get_template_part('parts/widget-tv-radios');
+
+$args = array('posts_per_page' => 3, 'cat' => $obj->term_id, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'DESC');
+$relateds = get_posts($args);
+if (!empty($relateds)) :
+?>
+    <div class="w-100 mt-4">
+        <h6 class="w-100 border-bottom border-3 pb-2 m-0 ps-widget--title"><i class="fa-solid fa-fire"></i> Mais lidas em <?php echo $obj->name; ?></h6>
+        <nav class="w-100 mt-3 border-bottom">
+            <?php foreach ($relateds as $related) : ?>
+                <div class="row mb-3">
+                    <div class="col-auto">
+                        <a href="<?php echo get_the_permalink($related->ID); ?>" title="<?php echo get_the_title($related->ID); ?>" class="d-block">
+                            <img src="<?php echo get_the_post_thumbnail_url($related->ID, 'ps-thumb-small') ?>" alt="<?php echo get_the_title($related->ID); ?>">
+                        </a>
+                    </div>
+                    <div class="col">
+                        <p class="font-tag"><?php echo get_the_tags($related->ID)[0]->name; ?></p>
+                        <h6 class="font-title">
+                            <a href="<?php echo get_the_permalink($related->ID); ?>" title="<?php echo get_the_title($related->ID); ?>"><?php echo get_the_title($related->ID); ?></a>
+                        </h6>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </nav>
+    </div>
+<?php
+endif;
+$ads_int  = get_field('ps_ads_internas', 'option');
+if ($ads_int) :
+?>
+    <div class="w-100 border p-2 bg-light d-flex justify-content-center mt-4">
+        <?php
+        shuffle($ads_int);
+        $ads_int = $ads_int[0];
+        if ($ads_int['ps_ads_internas_link']) {
+        ?>
+            <a href="<?php echo $ads_int['ps_ads_internas_link']; ?>" target="_blank" title="">
+                <img src="<?php echo $ads_int['ps_ads_internas_conteudo']; ?>" alt="Anúncio">
+            </a>
+        <?php } else { ?>
+            <img src="<?php echo $ads_int['ps_ads_internas_conteudo']; ?>" alt="Anúncio">
+    <?php }
+        echo "</div>";
+    endif; ?>
