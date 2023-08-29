bd306710e0f7c7606ee3ca960a64cc24dde81252
joao teodoro <joaotdn@gmail.com>
2023-08-29 06:14:16 -0300

    add videos reels component home

--- parts/home-reels.php
+++ parts/home-reels.php
@@ -21,7 +21,7 @@ if (!empty($one_minute_list)) :
     <div class="col-12 col-md-8 mb-4">
         <div class="w-100 bg-dark p-3 ps-one-minute">
             <h5 class="mb-3 pb-3 font-title d-inline-block w-100 ps-one-minute--title">
-                <a href="#" title="Ver todos os vídeos em Sertão em 1 minuto">
+                <a href="<?php echo get_term_link($one_minute->term_id, 'programas'); ?>" title="Ver todos os vídeos em Sertão em 1 minuto">
                     <span class="text-white"><i class="fa-solid fa-video opacity-50 d-inline-block me-2"></i> Sertão em</span>
                     <span class="text-danger"> 1 minuto</span>
                 </a>
@@ -64,6 +64,68 @@ if (!empty($one_minute_list)) :
                         endforeach;
                         ?>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+endif;
+$reels = get_term_by('name', 'videos curtos', 'programas');
+$reels_list = array();
+if ($reels) {
+    $reels_list = get_posts(
+        array(
+            'posts_per_page' => 6,
+            'post_type' => 'tvsertao',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'programas',
+                    'field' => 'term_id',
+                    'terms' => $reels->term_id,
+                )
+            )
+        )
+    );
+}
+if (!empty($reels_list)) :
+?>
+    <div class="col-12 col-md-4 mb-4">
+        <div class="w-100 ps-home-reels p-3 border-bottom border-4 border-dark">
+            <h5 class="ps-home-reels--header text-uppercase mb-3 pb-3">
+                <a href="#" title="Ver todos os Vídeos curtos">
+                    <i class="fa-solid fa-clock-rotate-left text-danger"></i> <strong>Vídeos curtos</strong>
+                </a>
+            </h5>
+            <div class="w-100 ps-home-reels--nav cycle-slideshow" data-cycle-fx="fade" data-cycle-timeout="0" data-cycle-prev=".ps-home-reels--nav-prev" data-cycle-next=".ps-home-reels--nav-next" data-loop="1" data-cycle-slides="> .ps-home-reels--nav-item">
+                <?php
+                foreach ($reels_list as $rell) :
+                    $code = get_field('ps_video_id', $rell->ID);
+                    if ($code) {
+                ?>
+                        <div class="ps-home-reels--nav-item w-100">
+                            <a href="#" title="" data-video-id="<?php echo $code; ?>" class="d-inline-block border border-3 border-danger w-100 position-relative">
+                                <img src="<?php echo "https://img.youtube.com/vi/{$code}/hqdefault.jpg"; ?>" alt="" class="w-100" data-bs-toggle="modal" data-bs-target="#videoHomeModal">
+                                <h6 class="d-flex position-absolute p-3">
+                                    <span class="d-block text-white flex-shrink-1">
+                                        <i class="fa-solid fa-play"></i>
+                                    </span>
+                                    <span class="d-block text-white ps-2 fw-bolder">
+                                        <?php echo $rell->post_title; ?>
+                                    </span>
+                                </h6>
+                            </a>
+                        </div>
+                <?php
+                    }
+                endforeach;
+                ?>
+                <div class="btn-group w-100 mt-3" role="group" aria-label="Basic outlined example">
+                    <a href="#" class="btn btn-outline-dark ps-home-reels--nav-prev">
+                        <i class="fa-solid fa-chevron-left"></i>
+                    </a>
+                    <a href="#" class="btn btn-outline-dark ps-home-reels--nav-next">
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </a>
                 </div>
             </div>
         </div>
--- src/js/components/oneMinute.js
+++ src/js/components/oneMinute.js
@@ -29,4 +29,12 @@
             $(this).find('iframe').attr('src', '');
         });
     }
+
+    if ($('.ps-home-reels').length) {
+        $('a', '.ps-home-reels--nav-item').on('click', function(e) {
+            e.preventDefault();
+            const code = $(this).attr('data-video-id');
+            $('iframe', '#videoHomeModal').attr('src', 'https://www.youtube.com/embed/' + code);
+        });
+    }
 })(jQuery);
